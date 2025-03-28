b71f2af88e10d497831a204bd62f56dabdbef0c8
shovan777 <raj.shrestha778@gmail.com>
2025-03-28 15:28:04 -0400

    Implement kalman filter algorithm for a 1d motion model.

--- src/include/linearMotion.h
+++ src/include/linearMotion.h
@@ -10,9 +10,17 @@ public:
     // Set a new transition matrix.
     void setTransitionMatrix(matrix *transition_matrix);
 
-    // Update the state using the input vector.
-    matrix *updateState(matrix *input_vec);
+    // Get the current transition matrix.
+    matrix *getTransitionMatrix();
 
+    // Update the state using the input vector.
+    void updateState(matrix *input_vec);
+
+    // Get the current state vector.
+    matrix *getStateVector() { return state_vec; }
+
+    // set the state vector
+    void setStateVector(matrix *state_vec);
     // Destructor: free the memory allocated for the matrices.
     ~LinearMotion();
 
