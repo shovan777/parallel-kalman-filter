73d12b293ff74f12a39ca35b1fa2579d0753cc28
aaw64 <aaw64@login3.crc.pitt.edu>
2025-04-25 11:36:30 -0400

    most recent correct writing code for plotting added

--- src/write.cpp
+++ src/write.cpp
@@ -2,29 +2,45 @@
 #include <fstream>
 #include "include/write.h"
 
-int writeDoubles(double * data)
+int writeDoubles(double data[][1000][8])
 {
-
-    
+      std::cout << "Writing started" << std::endl;
 
       std::ofstream myfile;
       myfile.open ("data.csv");
-      myfile << "This is the first cell in the first column.\n";
-      myfile << "a,b,c,\n";
-      myfile << "c,s,v,\n";
-      myfile << "1,2,3.456\n";
-      myfile << "semi;colon\n";
-      myfile << "data[0]\n";
-      myfile << data[0] << "\n";
-      myfile << "data all values:\n";
+      // myfile << "This is the first cell in the first column.\n";
+      // myfile << "a,b,c,\n";
+      // myfile << "c,s,v,\n";
+      // myfile << "1,2,3.456\n";
+      // myfile << "semi;colon\n";
+      // myfile << "data[0][0]\n";
+      // // myfile << data[0] << "\n";
+      // myfile << "data all values:\n";
 
-      //write first 20 values of data into file
+      // write first 8000 values of data into file
 
-      for (int i = 0; i < 20; i++)
-      {
-          myfile << data[i] << ",";
+      for (int k = 0; k < 10; k++) {
+
+        for (int i = 0; i < 8; i++) {
+
+          for (int j = 0; j < 1000; j++) {
+
+          
+            if (j != 999) {
+              myfile << data[k][j][i] << ",";
+            } else {
+                myfile << data[k][j][i];
+            }
+
+          }
+          myfile << std::endl;
+
+        }
+        myfile << "*" << std::endl << std::endl;
+
       }
 
       myfile.close();
+      std::cout << "WRITE SUCCESSFUL" << std::endl;
       return 0;
 }
